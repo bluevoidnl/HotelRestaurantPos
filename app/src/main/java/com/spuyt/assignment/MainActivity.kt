2b374a094e6d01dda3c5483482ae7e73cedf8815
Rob van der Veer <rob@bluevoid.nl>
2020-10-25 18:51:40 +0100

    camera and pixelation works, still needs layout and viewmodel improvements

--- app/src/main/java/com/spuyt/assignment/MainActivity.kt
+++ app/src/main/java/com/spuyt/assignment/MainActivity.kt
@@ -1,28 +1,236 @@
 package com.spuyt.assignment
 
-import androidx.appcompat.app.AppCompatActivity
-//import androidx.activity.viewModels
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.graphics.SurfaceTexture
+import android.hardware.camera2.*
 import android.os.Bundle
+import android.os.Handler
+import android.os.HandlerThread
+import android.util.Log
+import android.util.Size
+import android.util.SparseIntArray
+import android.view.Surface
+import android.view.TextureView
+import android.view.TextureView.SurfaceTextureListener
+import android.view.View
+import android.widget.Button
 import android.widget.ImageView
+import android.widget.SeekBar
+import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.lifecycle.observe
 import kotlin.concurrent.thread
+
 
 class MainActivity : AppCompatActivity() {
 
-//    private val viewModel: PixelateViewModel by viewModels(){
-//        PixelateViewModel()
-//    }
+    companion object {
+        const val REQUEST_CAMERA_PERMISSION = 200
+        const val TAG = "MainActivity"
+    }
+
+    val viewModel: PixelateViewModel by viewModels()
+
+    lateinit var textureView: TextureView
+    lateinit var takePictureButton: Button
+
+    var cameraDevice: CameraDevice? = null
+    lateinit var imageDimension: Size
+
+    // todo: check HandlerThread : to coroutine?
+    private var mBackgroundHandler: Handler? = null
+    private var mBackgroundThread: HandlerThread? = null
+
+    // todo: make more kotlin style
+    private val ORIENTATIONS: SparseIntArray = SparseIntArray()
+
+    init {
+        ORIENTATIONS.append(Surface.ROTATION_0, 90)
+        ORIENTATIONS.append(Surface.ROTATION_90, 0)
+        ORIENTATIONS.append(Surface.ROTATION_180, 270)
+        ORIENTATIONS.append(Surface.ROTATION_270, 180)
+    }
+
+    private val textureListener: SurfaceTextureListener = object : SurfaceTextureListener {
+        override fun onSurfaceTextureAvailable(surface: SurfaceTexture, width: Int, height: Int) {
+            openCamera()
+        }
+
+        override fun onSurfaceTextureSizeChanged(surface: SurfaceTexture, width: Int, height: Int) {
+            // Transform you image captured size according to the surface width and height
+        }
+
+        override fun onSurfaceTextureDestroyed(surface: SurfaceTexture): Boolean {
+            return false
+        }
+
+        override fun onSurfaceTextureUpdated(surface: SurfaceTexture) {}
+    }
+
+    private val stateCallback: CameraDevice.StateCallback = object : CameraDevice.StateCallback() {
+        override fun onOpened(camera: CameraDevice) {
+            // This is called when the camera is open
+            Log.e(TAG, "onOpened")
+            cameraDevice = camera
+            createCameraPreview(camera)
+        }
+
+        override fun onDisconnected(camera: CameraDevice) {
+            cameraDevice?.close()
+        }
+
+        override fun onError(camera: CameraDevice, error: Int) {
+            cameraDevice?.close()
+            cameraDevice = null
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        textureView = findViewById(R.id.texture)
+        textureView.surfaceTextureListener = textureListener
+        takePictureButton = findViewById(R.id.btn_takepicture)
+        takePictureButton.setOnClickListener {
+            val bitmap = textureView.bitmap
+            viewModel.setPhotoBitmap(bitmap)
+        }
+        val image = findViewById<ImageView>(R.id.pix)
+        viewModel.pixelatedImage.observe(this) { image.post { image.setImageBitmap(it) } }
+
+        val seekBar = findViewById<SeekBar>(R.id.seekBar)
+        // todo: set initial value correct
+
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+                Toast.makeText(this@MainActivity, "${seekBar.progress}", Toast.LENGTH_SHORT).show()
+                viewModel.setNrBLocks(seekBar.progress + 1) // +1 to avoid 0
+            }
+        })
     }
 
     override fun onResume() {
         super.onResume()
-        val image=findViewById<ImageView>(R.id.pix)
-        thread{
-            val bitmap=PixelateViewModel.pixelateTestImage(this)
-            image.post { image.setImageBitmap(bitmap) }
+        thread {
+            // todo: remove test code
+            viewModel.pixelateTestImage(this)
+        }
+        startBackgroundThread()
+        if (textureView.isAvailable) {
+            openCamera()
+        } else {
+            textureView.surfaceTextureListener = textureListener
+        }
+
+        // Add permission for camera and let user grant the permission
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this@MainActivity, arrayOf(Manifest.permission.CAMERA),
+                                              REQUEST_CAMERA_PERMISSION)
+            return
+        }
+    }
+
+    override fun onPause() {
+        Log.e(TAG, "onPause")
+        closeCamera()
+        stopBackgroundThread()
+        super.onPause()
+    }
+
+    private fun startBackgroundThread() {
+        mBackgroundThread = HandlerThread("Camera Background")
+        mBackgroundThread!!.start()
+        mBackgroundHandler = Handler(mBackgroundThread!!.looper)
+    }
+
+    private fun stopBackgroundThread() {
+        mBackgroundThread!!.quitSafely()
+        try {
+            mBackgroundThread!!.join()
+            mBackgroundThread = null
+            mBackgroundHandler = null
+        } catch (e: InterruptedException) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun openCamera() {
+        val manager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
+        Log.e(TAG, "is camera open")
+        try {
+            val cameraId = manager.cameraIdList[0]
+            val characteristics = manager.getCameraCharacteristics(cameraId)
+            val map = characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP)!!
+            imageDimension = map.getOutputSizes(SurfaceTexture::class.java)[0]
+
+            val permission = ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+            if (permission != PackageManager.PERMISSION_GRANTED) {
+                return
+            }
+            manager.openCamera(cameraId, stateCallback, null)
+        } catch (e: CameraAccessException) {
+            e.printStackTrace()
+        }
+        Log.e(TAG, "openCamera X")
+    }
+
+    fun createCameraPreview(camera: CameraDevice) {
+        try {
+            val texture = textureView.surfaceTexture!!
+            texture.setDefaultBufferSize(imageDimension.width, imageDimension.height)
+            val surface = Surface(texture)
+            val captureRequestBuilder = camera.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
+            captureRequestBuilder.addTarget(surface)
+            camera.createCaptureSession(listOf(surface), object : CameraCaptureSession.StateCallback() {
+                override fun onConfigured(cameraCaptureSession: CameraCaptureSession) {
+                    //The camera is already closed
+                    if (null == cameraDevice) {
+                        return
+                    }
+                    // When the session is ready start displaying the preview.
+                    captureRequestBuilder.set(CaptureRequest.CONTROL_MODE, CameraMetadata.CONTROL_MODE_AUTO)
+                    try {
+                        cameraCaptureSession.setRepeatingRequest(captureRequestBuilder.build(), null,
+                                                                 mBackgroundHandler)
+                    } catch (e: CameraAccessException) {
+                        e.printStackTrace()
+                    }
+                }
+
+                override fun onConfigureFailed(cameraCaptureSession: CameraCaptureSession) {
+                    Toast.makeText(this@MainActivity, "Configuration change", Toast.LENGTH_SHORT).show()
+                }
+            }, null)
+        } catch (e: CameraAccessException) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun closeCamera() {
+        cameraDevice?.let {
+            it.close()
+            cameraDevice = null
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String?>, grantResults: IntArray) {
+        if (requestCode == REQUEST_CAMERA_PERMISSION) {
+            if (grantResults[0] == PackageManager.PERMISSION_DENIED) {
+                // close the app
+                Toast.makeText(this, "Sorry!!!, you can't use this app without granting permission", Toast.LENGTH_LONG)
+                    .show()
+                finish()
+            }
         }
     }
 }
--- app/src/main/java/com/spuyt/assignment/PixelateViewModel.kt
+++ app/src/main/java/com/spuyt/assignment/PixelateViewModel.kt
@@ -6,79 +6,94 @@ import android.graphics.BitmapFactory
 import android.graphics.Color
 import androidx.core.graphics.get
 import androidx.core.graphics.set
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import java.io.InputStream
+import kotlin.concurrent.thread
 
 class PixelateViewModel : ViewModel() {
 
-    companion object {
-        fun pixelateTestImage(context: Context): Bitmap {
-            val bitmap: InputStream = context.assets.open("funny_car.jpg")
-            val image = BitmapFactory.decodeStream(bitmap)
-            return pixelate(image)
-        }
+    val pixelatedImage = MutableLiveData<Bitmap>()
+    //val pixelatedImage = MutableLiveData<Resource<Bitmap>>()
 
-        fun pixelate(image: Bitmap): Bitmap {
-            //BitmapFactory.decodeFile("assets/)
-            val W = image.width
-            val H = image.height
+    private var lastPhotoBitmap: Bitmap? = null
+    private var nrBlocks = 20
+    fun setPhotoBitmap(bitmap: Bitmap) {
+        lastPhotoBitmap = bitmap
+        pixelateImage()
+    }
 
-            val div = 30
-            val blockSize = W / div
-            val targetW = (W / blockSize)
-            val targetH = (H / blockSize)
-            //println("$W $H $blockSize target $targetW $targetH")
+    fun setNrBLocks(blockNr: Int) {
+        this.nrBlocks = blockNr
+        pixelateImage()
+    }
 
-            val targetImage = Bitmap.createBitmap(W, H, Bitmap.Config.ARGB_8888)
-            for (wIndex in 0 until targetW) {
-                for (hIndex in 0 until targetH) {
-                    createPixel(blockSize, wIndex, hIndex, image, targetImage)
-                }
-            }
-            return targetImage
-        }
-
-        private fun createPixel(
-            blockSize: Int,
-            wIndex: Int,
-            hIndex: Int,
-            image: Bitmap,
-            targetImage: Bitmap
-        ) {
-            var redSum = 0
-            var blueSum = 0
-            var greenSum = 0
-            val startX = wIndex * blockSize
-            val endX = blockSize + startX - 1
-            //println("$startX $endX ${image.width}")
-            for (x: Int in startX..endX) {
-                val startY = hIndex * blockSize
-                val endY = blockSize + startY - 1
-                //println("$startY $endY ${image.height}")
-                for (y: Int in startY..endY) {
-                    val intColor = image[x, y]
-                    redSum += Color.red(intColor)
-                    blueSum += Color.blue(intColor)
-                    greenSum += Color.green(intColor)
-                }
-            }
-            val pixelsInBlock = blockSize * blockSize
-            val blockColor = Color.argb(
-                255,
-                redSum / pixelsInBlock,
-                greenSum / pixelsInBlock,
-                blueSum / pixelsInBlock
-            )
-            for (x: Int in startX..endX) {
-                val startY = hIndex * blockSize
-                val endY = blockSize + startY - 1
-                //println("$startY $endY ${image.height}")
-                for (y: Int in startY..endY) {
-                    targetImage.set(x, y, blockColor)
-                }
+    private fun pixelateImage() {
+        lastPhotoBitmap?.let {
+            thread {
+                // todo: make lifecycle aware
+                // todo: cancel previous calculation when called again
+                val pixBitmap = pixelate(it, nrBlocks)
+                //pixelatedImage.postValue(Resource.Success(pixBitmap))
+                pixelatedImage.postValue(pixBitmap)
             }
         }
+    }
 
+   private fun pixelate(image: Bitmap, nrBlocks: Int): Bitmap {
+        //BitmapFactory.decodeFile("assets/)
+        val w = image.width
+        val h = image.height
 
+        // todo: fix rounding error
+        val blockSize = w / nrBlocks
+        val targetW = (w / blockSize)
+        val targetH = (h / blockSize)
+        //println("$W $H $blockSize target $targetW $targetH")
+        val targetImage = image.copy(Bitmap.Config.ARGB_8888, true)
+        // val targetImage = Bitmap.createBitmap(W, H, Bitmap.Config.ARGB_8888)
+        for (wIndex in 0 until targetW) {
+            for (hIndex in 0 until targetH) {
+                createPixel(blockSize, wIndex, hIndex, image, targetImage)
+            }
+            pixelatedImage.postValue(targetImage)
+        }
+        return targetImage
+    }
+
+    fun pixelateTestImage(context: Context) {
+        val bitmap: InputStream = context.assets.open("funny_car.jpg")
+        lastPhotoBitmap = BitmapFactory.decodeStream(bitmap)
+        pixelateImage()
+    }
+
+    private fun createPixel(blockSize: Int, wIndex: Int, hIndex: Int, image: Bitmap, targetImage: Bitmap) {
+        var redSum = 0
+        var blueSum = 0
+        var greenSum = 0
+        val startX = wIndex * blockSize
+        val endX = blockSize + startX - 1
+        //println("$startX $endX ${image.width}")
+        for (x: Int in startX..endX) {
+            val startY = hIndex * blockSize
+            val endY = blockSize + startY - 1
+            //println("$startY $endY ${image.height}")
+            for (y: Int in startY..endY) {
+                val intColor = image[x, y]
+                redSum += Color.red(intColor)
+                blueSum += Color.blue(intColor)
+                greenSum += Color.green(intColor)
+            }
+        }
+        val pixelsInBlock = blockSize * blockSize
+        val blockColor = Color.argb(255, redSum / pixelsInBlock, greenSum / pixelsInBlock, blueSum / pixelsInBlock)
+        for (x: Int in startX..endX) {
+            val startY = hIndex * blockSize
+            val endY = blockSize + startY - 1
+            //println("$startY $endY ${image.height}")
+            for (y: Int in startY..endY) {
+                targetImage[x, y] = blockColor
+            }
+        }
     }
 }
