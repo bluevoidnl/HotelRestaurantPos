7329f640ffabfcb7ec805b5d2d7777054cbb5391
Rob van der Veer <rob@bluevoid.nl>
2020-10-26 00:37:49 +0100

    use coroutines to make puxelation job cancallable

--- app/src/main/java/com/spuyt/assignment/PixelateViewModel.kt
+++ app/src/main/java/com/spuyt/assignment/PixelateViewModel.kt
@@ -9,14 +9,18 @@ import androidx.core.graphics.get
 import androidx.core.graphics.set
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.cancelAndJoin
+import kotlinx.coroutines.launch
 import java.io.InputStream
-import kotlin.concurrent.thread
 
 class PixelateViewModel : ViewModel() {
 
     val pixelationProgress = MutableLiveData<Int>()
     val pixelatedImage = MutableLiveData<Resource<Bitmap>>()
-
+    var pixelateJob: Job? = null
     private var lastPhotoBitmap: Bitmap? = null
     private var nrBlocks = 20
     fun setPhotoBitmap(bitmap: Bitmap) {
@@ -31,20 +35,25 @@ class PixelateViewModel : ViewModel() {
 
     private fun pixelateImage() {
         lastPhotoBitmap?.let {
-            thread {
-                // todo: make lifecycle aware
-                // todo: cancel previous calculation when called again
-                val pixBitmap = pixelate(it, nrBlocks)
-                pixelatedImage.postValue(Resource.Success(pixBitmap))
+            viewModelScope.launch(Dispatchers.Default) {
+                pixelateJob?.let {
+                    // cancel the current job if any and wait for completion
+                    it.cancelAndJoin()
+                }
+                // start pixelation job
+                pixelateJob = viewModelScope.launch(Dispatchers.Default) {
+                    val pixBitmap = pixelate(it, nrBlocks)
+                    pixelatedImage.postValue(Resource.Success(pixBitmap))
+                }
             }
         }
     }
 
-   private fun pixelate(image: Bitmap, nrBlocks: Int): Bitmap {
+    private fun pixelate(image: Bitmap, nrBlocks: Int): Bitmap {
         val w = image.width
         val h = image.height
 
-       Log.i("xxx","$w $h")
+        Log.i("xxx", "$w $h")
 
         val blockSize = w / nrBlocks
         val targetW = (w / blockSize)
@@ -52,9 +61,9 @@ class PixelateViewModel : ViewModel() {
         //println("$W $H $blockSize target $targetW $targetH")
         val targetImage = image.copy(Bitmap.Config.ARGB_8888, true)
         for (wIndex in 0 until targetW) {
-            var endX=0
-            for (hIndex in 0 until targetH+1) {
-                 endX=createPixel(blockSize, wIndex, hIndex, image, targetImage)
+            var endX = 0
+            for (hIndex in 0 until targetH + 1) {
+                endX = createPixel(blockSize, wIndex, hIndex, image, targetImage)
             }
             pixelatedImage.postValue(Resource.Loading(targetImage))
             pixelationProgress.postValue(endX)
@@ -72,7 +81,7 @@ class PixelateViewModel : ViewModel() {
         var redSum = 0
         var blueSum = 0
         var greenSum = 0
-        var pixelsInBlock=0
+        var pixelsInBlock = 0
         val startX = wIndex * blockSize
         val endX = blockSize + startX - 1
         //println("$startX $endX ${image.width}")
@@ -81,7 +90,7 @@ class PixelateViewModel : ViewModel() {
             val endY = blockSize + startY - 1
             //println("$startY $endY ${image.height}")
             for (y: Int in startY..endY) {
-                if(y<image.height) {
+                if (y < image.height) {
                     val intColor = image[x, y]
                     redSum += Color.red(intColor)
                     blueSum += Color.blue(intColor)
@@ -90,7 +99,7 @@ class PixelateViewModel : ViewModel() {
                 }
             }
         }
-        if(pixelsInBlock >0) {
+        if (pixelsInBlock > 0) {
             val blockColor = Color.argb(255, redSum / pixelsInBlock, greenSum / pixelsInBlock, blueSum / pixelsInBlock)
             for (x: Int in startX..endX) {
                 val startY = hIndex * blockSize
